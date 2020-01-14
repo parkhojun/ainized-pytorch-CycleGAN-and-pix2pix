58f239a019c7918da7a3013b5b33f40f62604877
parkhojun <mara8534@naver.com>
2020-01-14 19:07:41 +0900

    Update .js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'result')));
+app.use("/results", express.static(path.join(__dirname, 'results')));
 
 
 app.use('/', indexRouter);
