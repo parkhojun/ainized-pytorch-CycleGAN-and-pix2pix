0482d744a0e82fb455fbc2d498839915d378ae8e
parkhojun <mara8534@naver.com>
2020-01-20 12:38:26 +0900

    Update to show image results in swagger

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+var cors = require("cors");
+
+
 
 const indexRouter = require('./routes/index');
 const uploadRouter = require('./routes/upload');
@@ -9,6 +12,9 @@ const evaluationRouter = require('./routes/evaluation');
 
 const app = express();
 console.log(path.join(__dirname, 'public'))
+app.use(cors({
+    origin: 'https://ainize.ai',
+  }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
