e07f97f1733610a51f5046ab582a1872d55dde03
parkhojun <mara8534@naver.com>
2020-01-21 12:08:54 +0900

    Update to swagger.yaml

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -1,20 +1,57 @@
 let express = require('express');
 var exec = require('child_process').exec, child;
 // https://stackoverflow.com/questions/1880198/how-to-execute-shell-command-in-javascript
+const multer = require('multer');
+const uuid = require('uuid');
+
+
 let router = express.Router();
 
-/* GET home page. */
-/*router.get('/', (req, res, next) => {
-    console.log(11);
-    exec("python test.py --dataroot datasets/horse2zebra/testA --name horse2zebra_pretrained --model test --no_dropout", (error, stdout, stderr) => {
-        if (!error) {
-            res.json({ "msg": stdout });
-        } else {
-            res.json({ "msg": stderr });
-        }
-    });
-});*/
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploaded')
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuid.v4() + file.originalname)
+    }
+})
+const upload = multer({ storage: storage });
 
+
+function move_file_to_right_location(filename, option) {
+    
+    var final_location = "datasets/" + option + "/testA/";
+
+    if(option === 'maps'){
+        final_location = "datasets/"+ option +"/test/";
+    }
+    else if(option === 'facades'){
+        final_location = "datasets/"+ option +"/test/";
+    }
+    else if(option === 'night2day'){
+        final_location = "datasets/"+ option +"/test/";
+    }
+    else{
+        final_location = "datasets/"+ option +"/testA/";
+    }
+    
+    console.log(final_location);
+    const final_command = "mv ./uploaded/" + filename + " " + final_location;
+    console.log(final_command);
+    return new Promise((resolve, reject) => {
+        exec(final_command, (error, stdout, stderr) => {
+            console.log({stdout, stderr});
+            if (!error) {
+                resolve({stdout, stderr});
+            } else {
+                reject({error, stderr});
+            }
+        });
+    });
+};
+
+
+// WEB_UI EDITION = RUNNING ONLY (UPLOAD AND REDIRECTION REQUIRED!)
 router.get('/cyclegan', (req, res, next) => {
     const { option } = req.query;
     exec("python test.py --dataroot datasets/" + option + "/testA --name " + option + "_pretrained --model test --no_dropout", (error, stdout, stderr) => {
@@ -26,6 +63,41 @@ router.get('/cyclegan', (req, res, next) => {
     });
 });
 
+// SWAGGER EDITION = UPLOAD + RUNNING
+router.post('/cyclegan', upload.array('files', 1), (req, res, next) => {
+    const { files } = req;
+    const { option } = req.query;
+    if (!files) {
+        // FIXME: FILE UPLOAD ERROR!!!!!!!!!!!!
+        res.status(400);
+        res.json({"status": "file upload error"});
+    } else {
+        const filename = files[0].filename;
+        console.log({filename, option});
+        move_file_to_right_location(filename, option)
+        .then(() => {
+            exec("python test.py --dataroot datasets/" + option + "/testA --name " + option + "_pretrained --model test --no_dropout", (error, stdout, stderr) => {
+                if (!error) {
+                    // FIXME: SHOW SUCCESS FILE TO res!
+                    const filename_without_ext = filename.split('.')[0];
+                    const real_file_location = `results/${option}_pretrained/test_latest/images/${filename_without_ext}_fake_B.png`;
+                    res.download(real_file_location);
+                } else {
+                    res.status(500);
+                    res.json({"status": "ml test error"});
+                    // FIXME: TEST ERROR!!!!!!
+                }
+            });
+        })
+        .catch(() => {
+            res.status(500);
+            res.json({"status": "file move error"});
+        })
+    }
+
+});
+
+// WEB_UI EDITION = RUNNING ONLY (UPLOAD AND REDIRECTION REQUIRED!)
 router.get('/pix2pix', (req, res, next) => {
     const { option } = req.query;
     const real_locations = {
@@ -42,23 +114,46 @@ router.get('/pix2pix', (req, res, next) => {
         }
     });
 });
-/*
-router.get('/:image', (req, res, next) => {
-    const { image } = req.params;
-    const tag = image.split(".")[0].split("_").slice(1).join("-");
-    
-    console.log(image, tag);
 
-    const query = `bash run_single.sh "sample_data/images/${image}" "${tag}"`
-    exec(query, (error, stdout, stderr) => {
-        console.log(stdout);
-        if (!error) {
-            res.json({ "msg": stdout });
-        } else {
-            res.json({ "msg": stderr });
-        }
-    });
+// SWAGGER EDITION = UPLOAD + RUNNING
+router.post('/pix2pix', upload.array('files', 1), (req, res, next) => {
+    const { files } = req;
+    const { option } = req.query;
+    const real_locations = {
+        'maps': 'map2sat',
+        'facades': 'facades_label2photo',
+        'night2day': 'day2night',
+    }
+    console.log(11);
+    if (!files) {
+        // FIXME: FILE UPLOAD ERROR!!!!!!!!!!!!
+        res.status(400);
+        res.json({"status": "file upload error"});
+    } else {
+        const filename = files[0].filename;
+        console.log({filename, option});
+        move_file_to_right_location(filename, option)
+        .then(() => {
+            exec("python test.py --dataroot ./datasets/" + option + " --direction BtoA --model pix2pix --name " + real_locations[option] + "_pretrained", (error, stdout, stderr)  => {
+                if (!error) {
+                    // FIXME: SHOW SUCCESS FILE TO res!
+                    const filename_without_ext = filename.split('.')[0];
+                    const real_file_location = `results/${real_locations[option]}_pretrained/test_latest/images/${filename_without_ext}_fake_B.png`;
+                    res.download(real_file_location);
+                } else {
+                    res.status(500);
+                    res.json({"status": "ml test error"});
+                    // FIXME: TEST ERROR!!!!!!
+                    //console.log(error)
+                }
+            });
+        })
+        .catch(() => {
+            res.status(500);
+            res.json({"status": "file move error"});
+        })
+    }
 
 });
-*/
+
 module.exports = router;
